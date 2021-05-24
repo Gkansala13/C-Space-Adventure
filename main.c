583a4c6df6c88f875d13d9d3848ee4cfaa305978
Grayson Kansala <kansalag@oregonstate.edu>
2021-05-23 23:39:02 -0700

    Started refractoring code to cook book style

--- main.c
+++ main.c
@@ -7,14 +7,13 @@
 # include <stdio.h>
 # include <string.h>
 
+void printWelcome();
+char* responseTo();
+void printGreeting();
+
 int main(int argc, char** argv){
-    printf("Welcome to the Solar System! \n");
-    printf("There are 9 planets to explore. \n");
-    printf("What is your name? \n");
-    char name[32];
-    fgets(name, 32, stdin);
-    name[strcspn(name,"\n")]=0;
-    printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Alexa.\n", name);
+    printWelcome();
+    printGreeting(responseTo("What's your name?"));
     printf("Shall I randomly choose a planet for you to visit? (Y or N)\n");
     char chooseRandomPlanet[2];
     fgets(chooseRandomPlanet, 32, stdin);
@@ -22,4 +21,22 @@ int main(int argc, char** argv){
     printf("Choice is %s\n", chooseRandomPlanet);
     printf("Traveling to pluto... \n");
     printf("Arrived at PLuto, it's very cold here.\n");
+}
+
+void printWelcome() {
+    printf("Welcome to the Solar System!\n");
+    printf("There are 9 planets to explore.\n");
+}
+
+char* responseTo(char *astring) {
+    static char response[10];
+    printf("%s \n", astring);
+    fgets(response, sizeof(response), stdin);
+    response[strcspn(response, "\n")] =0;
+    return response;
+}
+
+void printGreeting(char *astring) {
+    printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Alexa.\n", astring);
+    
 }
--- main.h
+++ main.h
@@ -1,15 +1,6 @@
-#define SSTR_LEN 128
-#define STR_LEN 1024
-#define LSTR_LEN 4096
-
-typedef struct s_planet{
-    char* planet_name;
-    char* planet_desc;
-} t_planet;
-
-char *intro_sequence(void);
-void select_planet(char *);
-int random_planet(void);
-t_planet *get_planet_info(int);
-int valid_decision(char);
-int lookup_planet(char*);
+void printWelcome();
+char* responseFor();
+void printGreeting();
+void travelTo();
+void travelGuide();
+void travelToRandomPlanet();
